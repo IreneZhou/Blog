71fcd9bacb00431e10628dac1218a9291bfe01d8
IreneZhou <mengyizhou94@gmail.com>
2016-08-14 00:23:30 -0700

    add profile img

--- manage.py
+++ manage.py
@@ -12,6 +12,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role, Post=Post, Category=Category)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
